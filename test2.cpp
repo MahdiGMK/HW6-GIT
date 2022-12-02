ea9bec564151548b7fc6b81f12d3e04feac58370
MahdiGMK <30409029+MahdiGMK@users.noreply.github.com>
2022-12-02 20:48:14 +0330

    Master Edit

--- test2.cpp
+++ test2.cpp
@@ -5,6 +5,5 @@ void doAction()
     std::cout << "TEST2\n";
     std::cout << "new FUNC Update\n";
 
-    std::cout << "UPDATE IN MASTER";
-    std::cout << "Master Commit 2";
+    std::cout << "Master Edit";
 }
